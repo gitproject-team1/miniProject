df16c7071e56cab3add7d70d2c09b701a7bfd934
1myeji <106291546+1myeji@users.noreply.github.com>
2022-11-26 19:56:56 +0900

    feat: submit, clear items button

--- ImYeJi/GroceryBud/main.js
+++ ImYeJi/GroceryBud/main.js
@@ -1,63 +1,74 @@
-// 초기화 코드
-const alertEl = document.querySelector('.alert')
-const formEl = document.querySelector('.grocery-form')
-const inputEl = document.querySelector('#grocery')
-const submitBtnEl = document.querySelector('.submit-btn')
-const containerEl = document.querySelector('.grocery-container')
-const listEl = document.querySelector('.grocery-list')
-const deleteBtnEl = document.querySelector('.delete-btn')
-const clearBtnEl = document.querySelector('.clear-btn')
+const form = document.querySelector('.grocery-form')
+const input = document.querySelector('#grocery')
+const alert = document.querySelector('.alert')
+const list = document.querySelector('.grocery-list')
+const container = document.querySelector('.grocery-container')
+const clearBtn = document.querySelector('.clear-btn')
+const submitBtn = document.querySelector('.submit-btn')
 
-// list 추가
-submitBtnEl.addEventListener('click', () => {
-  alert()
-})
+let editFlag = false
+let editElement
+let editID = ''
 
-inputEl.addEventListener('keydown', function(e) {
-  if(e.keyCode === 13) {
-    e.preventDefault()
-    alert()
+
+// submit 
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+  const value = input.value
+  const id = new Date().getTime().toString()
+  if(value && !editFlag) {
+    const el = document.createElement('article')
+    el.classList.add('grocery-item')
+    el.setAttribute('data-id', id)
+    el.innerHTML = /* html */`
+    <p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  `
+  list.appendChild(el)
+  container.classList.add('show-container')
+  setBackToDefault()
+  // localstorage
+  const storage = {id: id, value: value}
+  let items = localStorage.getItem('list')? JSON.parse(localStorage.getItem('list')) : []
+  items.push(storage)
+  localStorage.setItem('list', JSON.stringify(items))
+  } else if(value && editFlag) {
+
+  } else {
+    alert.textContent = 'please enter value'
+    alert.classList.add('alert-danger')
+
+    setTimeout(function() {
+      alert.textContent = ''
+      alert.classList.remove('alert-danger')
+    }, 1000)
   }
 })
 
+// clear items 클릭
+clearBtn.addEventListener('click', function() {
+  const items = document.querySelectorAll('.grocery-item')
 
-
-
-
-// Functions
-function makeGroceryList (items) {
-  const el = document.createElement('div') 
-  el.classList.add('grocery-list')
-  el.innerHTML = /* html */ `
-  <article class="grocery-item">
-    <p class="title">${items}</p>
-      <div class="btn-container">
-        <button type="button" class="edit-btn">
-          <i class="fas fa-edit"></i>
-        </button>
-        <button type="button" class="delete-btn">
-          <i class="fas fa-trash"></i>
-        </button>
-      </div>
-  </article>
-`
-  containerEl.append(el)
-}
-
-function alert () {
-  items = inputEl.value
-
-  if(items === '') {
-    alertEl.textContent = 'Empty Value'
-    alertEl.style.backgroundColor = 'tomato'
-    setTimeout(()=> {
-      alertEl.textContent = ''
-      alertEl.style.backgroundColor = ''
-    }, 2000)
-  } 
-  
-  else {
-    makeGroceryList(items)
-    inputEl.value = ''
+  if(items.length > 0) {
+    for(const x of items) {
+      list.removeChild(x)
+    }
   }
+  container.classList.remove('show-container')
+  setBackToDefault()
+  localStorage.removeItem('list')
+})
+
+function setBackToDefault () {
+  input.value = ''
+  editFlag = false
+  editID = ''
+  submitBtn.text = 'submit'
 }
